4648bf83b4b0d6e223df872ad7e3140d0bb9b0b6
Richard <richard.yang.tong@gmail.com>
2021-03-10 14:46:53 -0500

    Seperated exercise out as a different schema.

--- models/workout.js
+++ models/workout.js
@@ -1,25 +1,50 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ExerciseSchema = new Schema({
+    exercises: [
+        {
+            type: {
+                type: String,
+                enum: ['resistence', 'cardio']
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+
+            duration: {
+                type: Number,
+                required: true
+            },
+
+            weight: {
+                type: Number,
+                required: true
+            },
+
+            reps: {
+                type: Number,
+                required: true
+            },
+            
+            sets: {
+                type: Number,
+                required: true
+            },
+        }
+    ]
+});
+
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
         defualt: Date.now
     },
-    exercise: [
-        {
-            type: String,
-            name: String,
-            duration: Number,
-            weight: Number,
-            reps: Number,
-            sets: Number
-        }
-    ],
-    totalDuration: {
-        type: Number,
-        default: 0
-    }
+    
+    exercises: [ExerciseSchema]
 });
 
 const Workout = mongoose.model('Workout', WorkoutSchema);

6ebb7c31c1a71f8dc135cf62d61bd03ccb041ad0
Richard <richard.yang.tong@gmail.com>
2021-03-09 21:03:35 -0500

    Added the middlewares for servering static files.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {

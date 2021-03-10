7ce06bef6479a369502040aeadd53afdc81d884f
Richard <richard.yang.tong@gmail.com>
2021-03-10 14:32:14 -0500

    Added a schema for day.

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({});
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        defualt: Date.now
+    }
+});
 
 const Workout = mongoose.model('Workout', WorkoutSchema);
 module.exports = Workout;

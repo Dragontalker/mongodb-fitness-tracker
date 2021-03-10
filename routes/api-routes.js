8039e3a34a65a7ada3fb49635794932fca139846
Richard <richard.yang.tong@gmail.com>
2021-03-10 11:01:46 -0500

    Connected the database with get request handler for showing all entires in the database.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const router = express.Router();
 
+const db = require('../models');
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
 router.get('/', (req, res) => {
-    res.json({msg: 'placeholder'});
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 module.exports = router;

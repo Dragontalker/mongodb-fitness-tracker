ec3dace62e18e7cdcf3c8bf15d72363cacde80cc
Richard <richard.yang.tong@gmail.com>
2021-03-09 21:09:08 -0500

    Mounted the api middleware to server and tested with a placeholder json.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({msg: 'placeholder'});
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use('/api', require('./routes/api-routes'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {

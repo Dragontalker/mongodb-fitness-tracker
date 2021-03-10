ad9dd675f688fb4cd1ccd7c28c5197271fca0d0e
Richard <richard.yang.tong@gmail.com>
2021-03-10 09:37:09 -0500

    Added an empty mongoose schema model.

---
+++ models/workout.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+module.exports = Workout;

ede38b450feb77bb2e56d11f672fdda5a60d12c5
Richard <richard.yang.tong@gmail.com>
2021-03-11 11:09:51 -0500

    Testing Heroku.

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 const express = require('express');
-const mongoose = require("mongoose");
 
 const app = express();
-
-// connect to MongoDB
-
-// mongoose.connect(
-//     process.env.MONGODB_URI || 'mongodb://localhost/workout',
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex: true,
-//       useFindAndModify: false
-//     }
-// );
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

81f764b3ee19a8fbe640951cb691dce932131155
Richard <richard.yang.tong@gmail.com>
2021-03-11 14:33:53 -0500

    Test Heroku again.

--- server.js
+++ server.js
@@ -22,19 +22,6 @@ app.use(express.static("public"));
 app.use('/', require('./routes/html-routes'));
 app.use('/api/workouts', require('./routes/api-routes'));
 
-// connect to MongoDB
-const mongoose = require('mongoose');
-
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-);
-
 
 const PORT = process.env.PORT || 3000;
 

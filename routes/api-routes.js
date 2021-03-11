37201f451c5c666fd0764fcabe9d294f9d8efdb6
Richard <richard.yang.tong@gmail.com>
2021-03-11 12:44:45 -0500

    Disabled api routes and test database connection.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -21,38 +21,38 @@ mongoose.connect(
 
 
 
-router.get('/', async (req, res) => {
-    const result = await db.Workout.find({})
-    res.json(result);
-});
+// router.get('/', async (req, res) => {
+//     const result = await db.Workout.find({})
+//     res.json(result);
+// });
 
-router.post('/', async (req, res) => {
-    const result = await db.Workout.create({});
-    res.json(result);
-});
+// router.post('/', async (req, res) => {
+//     const result = await db.Workout.create({});
+//     res.json(result);
+// });
 
-router.get('/range', async (req, res) => {
-    await db.Workout.deleteMany({'totalDuration': 0});
-    await db.Workout.deleteMany({'exercises': {$elemMatch: {'duration': 0}}});
-    const result = await db.Workout.find({}).sort({day: -1}).limit(7);
-    const reverse = result.reverse();
-    res.json(reverse);
-});
+// router.get('/range', async (req, res) => {
+//     await db.Workout.deleteMany({'totalDuration': 0});
+//     await db.Workout.deleteMany({'exercises': {$elemMatch: {'duration': 0}}});
+//     const result = await db.Workout.find({}).sort({day: -1}).limit(7);
+//     const reverse = result.reverse();
+//     res.json(reverse);
+// });
 
-router.put('/:id', async (req, res) => {
-    const id = req.params.id;
-    const data = req.body;
-    const duration = data.duration;
-    const workout = await db.Workout.findOneAndUpdate(
-        { _id: id },
-        { 
-            $push: { exercises: data },
-            $inc: { totalDuration: duration}
-        }, 
-        {new: true}
-    );
-    const result = await workout.save();
-    res.json(result);
-});
+// router.put('/:id', async (req, res) => {
+//     const id = req.params.id;
+//     const data = req.body;
+//     const duration = data.duration;
+//     const workout = await db.Workout.findOneAndUpdate(
+//         { _id: id },
+//         { 
+//             $push: { exercises: data },
+//             $inc: { totalDuration: duration}
+//         }, 
+//         {new: true}
+//     );
+//     const result = await workout.save();
+//     res.json(result);
+// });
 
 module.exports = router;
